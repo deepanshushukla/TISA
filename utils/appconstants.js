3372f2e5afea74488e4b8ba1fafddd83a6e5d991
deepanshu shukla <deepanshu.shukla@paytm.com>
2016-01-21 14:52:55 +0530

    added mongo

--- utils/appconstants.js
+++ utils/appconstants.js
@@ -2,5 +2,7 @@
  * Created by Deepanshu on 11/27/2015.
  */
 module.exports = {
-    mongourl : 'mongodb://localhost:27017/tisa'
+    //mongourl : 'mongodb://localhost:27017/tisa'
+    mongourl : 'mongodb://tisa:appozon@ds047065.mongolab.com:47065/tisa',
+
 }
